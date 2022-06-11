cba87a6b3fe9f87da65c74f1754ad1fe38ccf633
sky_haihai <skyhaihai2000@gmail.com>
2022-06-11 13:36:37 -0400

    camera svc added

---
+++ Services/TBT/CameraSvc.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public static class CameraSvc
+{
+    public static void SwitchCamera() {
+        
+    }
+}

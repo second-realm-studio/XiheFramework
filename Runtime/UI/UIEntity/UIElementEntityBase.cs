dcb30f6108befd620cc658d82fbc9a4a67486597
haihai home <skyhaihai2000@gmail.com>
2025-09-04 03:06:34 -0600

    bug fix

--- Runtime/UI/UIEntity/UIElementEntityBase.cs
+++ Runtime/UI/UIEntity/UIElementEntityBase.cs
@@ -5,11 +5,16 @@ namespace XiheFramework.Runtime.UI.UIEntity {
     public abstract class UIElementEntityBase : GameEntityBase {
         public override string GroupName => "UIElementEntity";
 
+        protected RectTransform rectTransform;
+
         protected override void OnInitCallback() {
             base.OnInitCallback();
 
             if (transform is not RectTransform) {
-                gameObject.AddComponent<RectTransform>();
+                rectTransform = gameObject.AddComponent<RectTransform>();
+            }
+            else {
+                rectTransform = transform as RectTransform;
             }
         }
     }
